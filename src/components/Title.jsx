c517308152c1a1f56c545acb71ae3d01a35abdd9
codebymatt <mattcraig365@gmail.com>
2017-11-08 13:43:42 +0000

    Fix bug where too many digits being displayed. Again.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,7 +6,7 @@ export default class Title extends React.Component {
             <div>
                 <h1>Newton</h1>
                 <div id='beta'>
-                    <p>Beta</p>
+                    <p>BETA</p>
                 </div>
             </div>
         );
